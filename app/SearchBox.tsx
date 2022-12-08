ee1b18c92ace01fc8f35cecb5fcbde4d42e4796b
AayushMohan <aayushmohan1702@gmail.com>
2022-12-08 14:40:23 +0530

    Styled Search Button

--- app/SearchBox.tsx
+++ app/SearchBox.tsx
@@ -13,7 +13,11 @@ const SearchBox = () => {
         placeholder="Search Keywords..."
         className="w-full h-14 rounded-sm placeholder-gray-500 text-gray-500 outline-none flex-1 bg-transparent dark:text-orange-400"
       />
-      <button type="submit" disabled={!input}>
+      <button
+        type="submit"
+        disabled={!input}
+        className="text-orange-400 disabled:text-gray-400"
+      >
         Search
       </button>
     </form>
