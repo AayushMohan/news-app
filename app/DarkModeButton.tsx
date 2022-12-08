06d3e53dc3469625409c68b65f9d2b6ec652183a
AayushMohan <aayushmohan1702@gmail.com>
2022-12-08 23:47:32 +0530

    Added UseEffect Hook

--- app/DarkModeButton.tsx
+++ app/DarkModeButton.tsx
@@ -1,6 +1,17 @@
 "use client";
 
+import { useState, useEffect } from "react";
+import { useTheme } from "next-themes";
+import { SunIcon, MoonIcon } from "@heroicons/react/24/solid";
+
 const DarkModeButton = () => {
+  const [mounted, setMounted] = useState(false);
+  const { systemThemes, theme, setTheme } = useTheme();
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
   return <div>DarkModeButton</div>;
 };
 
