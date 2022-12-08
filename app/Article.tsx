4ae37fdc1d55e20588ce7a101591e45b1c29ffc5
AayushMohan <aayushmohan1702@gmail.com>
2022-12-08 23:05:33 +0530

    Styled Footer

--- app/Article.tsx
+++ app/Article.tsx
@@ -22,7 +22,7 @@ const Article = ({ article }: Props) => {
           </section>
         </div>
 
-        <footer>
+        <footer className="text-xs text-right ml-auto flex space-x-1 pt-5 italic text-gray-500">
           <p>{article.source} - </p>
           <p>{article.published_at}</p>
         </footer>
