5ed7dcf463abb392d8ddcf88b91a36f00916d722
AayushMohan <aayushmohan1702@gmail.com>
2022-12-08 14:29:55 +0530

    Styled SearchBox

--- app/SearchBox.tsx
+++ app/SearchBox.tsx
@@ -1,7 +1,10 @@
-import React from "react";
-
 const SearchBox = () => {
-  return <div>SearchBox</div>;
+  return (
+    <form className="max-w-6xl mx-auto flex justify-between items-center px-5">
+      <input type="text" className="flex-1" />
+      <button type="submit">Search</button>
+    </form>
+  );
 };
 
 export default SearchBox;
