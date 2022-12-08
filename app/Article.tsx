3176442664fb5635abdd91a1002138b420a28fe8
AayushMohan <aayushmohan1702@gmail.com>
2022-12-08 22:54:51 +0530

    Styled Article section and added line clamp

--- app/Article.tsx
+++ app/Article.tsx
@@ -4,21 +4,21 @@ type Props = {
 
 const Article = ({ article }: Props) => {
   return (
-    <article>
+    <article className="bg-slate-100 dark:bg-slate-800 flex flex-col rounded-lg shadow-sm hover:scale-105 hover:shadow-lg hover:bg-slate-200 transition-all duration-200 ease-out">
       {article.image && (
         <img
           src={article.image}
           alt={article.title}
-          className="h-56 w-full object-cover rounded-lg shadow-md"
+          className="h-56 w-full object-cover rounded-t-lg shadow-md"
         />
       )}
 
-      <div>
-        <div>
-          <h2>{article.title}</h2>
+      <div className="flex-1 flex flex-col">
+        <div className="flex-1 flex flex-col p-5">
+          <h2 className="font-bold font-serif">{article.title}</h2>
 
-          <section>
-            <p>{article.description}</p>
+          <section className="mt-2 flex-1">
+            <p className="text-xs line-clamp-6">{article.description}</p>
           </section>
         </div>
 
