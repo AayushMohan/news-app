cb9154f997bf8cfd590583f5081dda5bb5ce52d1
AayushMohan <aayushmohan1702@gmail.com>
2022-12-08 14:24:17 +0530

    Completed NavLinks Section

--- app/NavLink.tsx
+++ app/NavLink.tsx
@@ -8,7 +8,13 @@ type Props = {
 
 const NavLink = ({ category, isActive }: Props) => {
   return (
-    <Link href={`/news/${category}`} className="navLink">
+    <Link
+      href={`/news/${category}`}
+      className={`navLink ${
+        isActive &&
+        "underline decoration-orange-400 underline-offset-4 font-bold text-lg"
+      }`}
+    >
       {category}
     </Link>
   );
