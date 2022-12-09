fd3e6bee2a1db5d149fc365b84dd699f01aae9e8
AayushMohan <aayushmohan1702@gmail.com>
2022-12-09 20:59:44 +0530

    Added Articles Page Details

--- app/article/page.tsx
+++ app/article/page.tsx
@@ -16,7 +16,7 @@ const ArticlePage = ({ searchParams }: Props) => {
 
   return (
     <div>
-      <section>
+      <section className="flex flex-col lg:flex-row pb-24 px-0 lg:px-10">
         {article.image && (
           <img
             className="h-50 max-w-md mx-auto md:max-w-lg lg:max-w-xl object-cover rounded-lg shadow-md"
@@ -24,6 +24,20 @@ const ArticlePage = ({ searchParams }: Props) => {
             alt={article.title}
           />
         )}
+
+        <div className="px-10">
+          <h1 className="headerTitle px-0 no-underline pb-2">
+            {article.title}
+          </h1>
+
+          <div className="flex divided-x-2 space-x-4">
+            <h2 className="font-bold">By: {article.author}</h2>
+            <h2 className="font-bold pl-4">Source: {article.source}</h2>
+            <p className="pl-4">{article.published_at}</p>
+          </div>
+
+          <p className="pt-4">{article.description}</p>
+        </div>
       </section>
     </div>
   );
